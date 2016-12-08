72a8226038b2ea1184740e860103da62a5510e04
Sylwester Milewski <Sylwester_Milewski@mentor.com>
2016-12-08 12:49:09 +0100

    day 6

---
+++ day6/task2.py
@@ -0,0 +1,19 @@
+import sys
+from collections import Counter
+
+numberofLetters = 8
+data = open('input', 'r')
+
+counters = list()
+for i in range(numberofLetters):
+    counters.append(Counter())
+
+for line in data:
+    for i in range(numberofLetters):
+        counters[i].update(line[i])
+
+for i in range(numberofLetters):
+    sys.stdout.write(str(counters[i].most_common()[-1][0]))
+
+
+
